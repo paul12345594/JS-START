72339c21e4b00cebc74c7ee8664012a76d408255
unknown <dungcapaul6@gmail.com>
2024-11-16 15:27:55 +0800

    START

---
+++ server.js
@@ -0,0 +1,82 @@
+//You should already know:
+//HTML, CSS, and Javascript 
+//Possibly experience with other libraries and frameworks 
+
+// How NodeJS differs from Vanilla JS
+// 1) Node runs on a server - not in a browser (backend not frontend)
+// 2) The console is the terminal window 
+console.log('PAULWORX')
+// 3) Global object instead of window object 
+//console.log(global);
+// 4) Has Common Core modules that we will explore 
+// 5) CommonJS modules instead of ES6 modules
+
+const os = require('os');
+const path = require('path')
+const { add, subtract, multiply, divide } = require ('./math')
+
+
+console.log(add(2, 3))
+console.log(subtract(2, 3))
+console.log(multiply(2, 3))
+console.log(divide(2, 3))
+
+
+
+
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+// console.log(__dirname)
+// console.log(__filename)
+
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
+
+// console.log(path.parse(__filename))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 5:07:41 
+// https://www.youtube.com/watch?v=f2EqECiTBL8
+// https://www.youtube.com/watch?v=BpXaVQSdo_E   CODE WITH ARJUN    
+
+
+
+
